32c2d37b3b38e720a94d59736f6f1a76eb460b7f
Jesufemi-A <jesufemia1@gmail.com>
2023-08-09 12:07:24 +0100

    added and updated BaseModel class for task 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This contains BaseModel class from which other classes will inherit
+"""
+import uuid
+import datetime
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        initialises the instance attributes everytime an object
+        is created
+
+        args:
+        self - refers to the instance itself
+        """
+
+        if kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at":
+                        self.created_at = datetime.datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    elif key == "updated_at":
+                        self.updated_at = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        self.key = value
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+
+    def __str__(self):
+        """
+        returns a human readeable representation of the object
+        """        
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        self.__dict__["id"] = str(uuid.uuid4())
+        return self.__dict__
