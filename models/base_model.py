f29af0a6192f8236e1ae0af079f078b33d28015d
Jesufemi-A <jesufemia1@gmail.com>
2023-08-16 00:06:22 +0100

    fixing pycode errors

--- models/base_model.py
+++ models/base_model.py
@@ -20,20 +20,18 @@ class BaseModel:
         args:
         self - refers to the instance itself
         """
-        val = "%Y-%m-%dT%H:%M:%S.%f"
+        date_f = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
-                c = "created_at"
-                u = "updated_at"
                 if key != "__class__":
                     if key == "created_at":
-                        self.created_at = datetime.strptime(kwargs[c], val)
+                        self.created_at = datetime.strptime(value, date_f)
                     elif key == "updated_at":
-                        i self.updated_at = datetime.strptime(kwargs[u], val)
+                        i self.updated_at = datetime.strptime(value, date_f)
                     else:
                         setattr(self, key, value)
         models.storage.new(self)
