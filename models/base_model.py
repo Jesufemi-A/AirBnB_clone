68d463329632c1bfd85200aacd2a5c389633f20c
Jesufemi-A <jesufemia1@gmail.com>
2023-08-16 00:03:11 +0100

    fixing pycode errors

--- models/base_model.py
+++ models/base_model.py
@@ -20,17 +20,20 @@ class BaseModel:
         args:
         self - refers to the instance itself
         """
+        val = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
+                c = "created_at"
+                u = "updated_at"
                 if key != "__class__":
                     if key == "created_at":
-                        self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        self.created_at = datetime.strptime(kwargs[c], val)
                     elif key == "updated_at":
-                        self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        i self.updated_at = datetime.strptime(kwargs[u], val)
                     else:
                         setattr(self, key, value)
         models.storage.new(self)
@@ -51,7 +54,8 @@ class BaseModel:
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all
+        keys/values of __dict__ of the instance
         """
         base_dict = self.__dict__.copy()
         base_dict["__class__"] = self.__class__.__name__
