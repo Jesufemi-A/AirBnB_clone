d35a92ea72217833b35de4285e7fdee026042ec8
Jesufemi-A <jesufemia1@gmail.com>
2023-08-15 23:37:17 +0100

    fixing to_dict method errors

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 This contains BaseModel class from which other classes will inherit
 """
 import uuid
-import datetime
+from datetime import datetime
 import models
 
 
@@ -21,16 +21,16 @@ class BaseModel:
         self - refers to the instance itself
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at":
-                        self.created_at = datetime.datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                     elif key == "updated_at":
-                        self.updated_at = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                        self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                     else:
                         setattr(self, key, value)
         models.storage.new(self)
@@ -46,15 +46,16 @@ class BaseModel:
         updates the public instance attribute updated_at
         with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        self.__dict__["id"] = self.id
-        return self.__dict__
+        base_dict = self.__dict__.copy()
+        base_dict["__class__"] = self.__class__.__name__
+        base_dict["created_at"] = self.created_at.isoformat()
+        base_dict["updated_at"] = self.updated_at.isoformat()
+        base_dict["id"] = self.id
+        return base_dict
